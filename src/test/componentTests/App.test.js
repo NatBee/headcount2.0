f3ec20c08ce428745cb0ba21ebad83577218b338
Ricardo Viera <ricardoclean@gmail.com>
2018-01-28 18:50:53 -0700

    More updates to tests

--- src/test/componentTests/App.test.js
+++ src/test/componentTests/App.test.js
@@ -67,17 +67,24 @@ describe('App tests', () => {
 
   })
 
-  it.only('should compare the cards when there are 2', () => {
-    //need to create mock data 
+  it('should compare the cards when there are 2', () => {
+    const mockedCompareData = 
+      {data: {2004: 0.24, 2005: 0.278, 2006: 0.337, 2007: 0.395, 2008: 0.536, 2009: 0.598, 2010: 0.64, 2011: 0.672, 2012: 0.695, 2013: 0.703, 2014: 0.741}
+location: "COLORADO"}, 
+{data: {2004: 0.069, 2005: 0.509, 2006: 0.638, 2007: 0.994, 2008: 0.992, 2009: 1, 2010: 0.993, 2011: 0.994, 2012: 0.993, 2013: 0.989, 2014: 0.994}
+location: "COLORADO SPRINGS 11"};
+
     renderedComponent.instance().comparison(['COLORADO', 'COLORADO SPRINGS 11'])
     console.log(renderedComponent.state().comparisonData)
-    expect(renderedComponent.state()).toEqual(mockedDistrict)
+    expect(renderedComponent.state().comparisonData).toEqual(mockedCompareData)
   })
 
   it('it should remove a card from cardCompData array', () => {
-    //need to create mock data 
-    renderedComponent.instance().removeComparisonCard('COLORADO', 'COLORADO SPRINGS 11')
-    
+    const mockedCompareData = {
+      {data: {2004: 0.24, 2005: 0.278, 2006: 0.337, 2007: 0.395, 2008: 0.536, 2009: 0.598, 2010: 0.64, 2011: 0.672, 2012: 0.695, 2013: 0.703, 2014: 0.741}
+location: "COLORADO"}
+    renderedComponent.instance().removeComparisonCard('COLORADO SPRINGS 11')
+    expect(renderedComponent.state().comparisonData).toEqual(mockedCompareData)
   })
 
 })
